d9da4d4a9d7a92f581b1f418a9970bc0fde8b544
Taylor Powell <taylorpowell@Taylors-MacBook-Pro-4.local>
2020-08-12 12:45:36 -0400

    initial xcode proj creation

---
+++ vidapp-onedaybuild/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vidapp-onedaybuild
+//
+//  Created by Taylor Powell on 8/12/20.
+//  Copyright © 2020 Taylor Wysong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
