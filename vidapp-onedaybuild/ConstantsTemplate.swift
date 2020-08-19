ad6285c5de5087d2996beeb2d84af878b55f6285
Taylor Powell <taylorpowell@Taylors-MacBook-Pro-4.local>
2020-08-19 12:01:12 -0400

    Constants Template file added

---
+++ vidapp-onedaybuild/ConstantsTemplate.swift
@@ -0,0 +1,22 @@
+//
+//  ConstantsTemplate.swift
+//  vidapp-onedaybuild
+//
+//  Created by Taylor Powell on 8/19/20.
+//  Copyright © 2020 Taylor Wysong. All rights reserved.
+//
+
+/* Add your own keys below and uncomment the code below */
+
+import Foundation
+
+
+/*
+struct Constants {
+    
+    //Make sure keep API key safe
+    static var API_KEY = ""
+    static var PLAYLIST_ID = ""
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
+*/
