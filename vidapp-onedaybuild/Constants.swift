688d8be4c128534cbc8151bfd58e593bb1980436
Taylor Powell <taylorpowell@Taylors-MacBook-Pro-4.local>
2020-08-19 10:58:33 -0400

    set up URL's and networking configuration to pull video snippet information

---
+++ vidapp-onedaybuild/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  vidapp-onedaybuild
+//
+//  Created by Taylor Powell on 8/16/20.
+//  Copyright © 2020 Taylor Wysong. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    static var API_KEY = ""
+    static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
+    static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+}
---
+++ vidapp-onedaybuild/Video.swift
@@ -0,0 +1,18 @@
+//
+//  Video.swift
+//  vidapp-onedaybuild
+//
+//  Created by Taylor Powell on 8/19/20.
+//  Copyright © 2020 Taylor Wysong. All rights reserved.
+//
+
+import Foundation
+
+struct Video {
+    
+    var videoID = ""
+    var title = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
