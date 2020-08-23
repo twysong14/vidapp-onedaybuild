4a6d25f34de034a6fd7ee7f735f91c7f70bdc6f7
Taylor Powell <taylorpowell@Taylors-MacBook-Pro-4.local>
2020-08-22 22:31:15 -0400

    added code to parse JSON response

---
+++ vidapp-onedaybuild/Response.swift
@@ -0,0 +1,27 @@
+//
+//  Response.swift
+//  vidapp-onedaybuild
+//
+//  Created by Taylor Powell on 8/22/20.
+//  Copyright © 2020 Taylor Wysong. All rights reserved.
+//
+
+import Foundation
+
+struct Response: Decodable {
+    
+    var items:[Video]?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case items
+        
+    }
+    
+    init(from decoder: Decoder) throws {
+        
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        
+        self.items = try container.decode([Video].self, forKey: .items)
+    }
+}
