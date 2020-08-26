53be85ef55617a610f64b02d31c35d565dedb72a
Taylor Powell <taylorpowell@Taylors-MacBook-Pro-4.local>
2020-08-26 11:37:48 -0400

    added detailviewcontroller, segue to detail, and webview in detail view controller

--- vidapp-onedaybuild/Helpers/Constants.swift
+++ vidapp-onedaybuild/Helpers/Constants.swift
@@ -15,4 +15,5 @@ struct Constants {
     static var PLAYLIST_ID = "UU2D6eRvCeMtcF5OGHf1-trw"
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
     static var VIDEOCELL_ID = "VideoCell"
+    static var YT_EMBED_URL = "https://www.youtube.com/embed/"
 }
--- vidapp-onedaybuild/Helpers/ConstantsTemplate.swift
+++ vidapp-onedaybuild/Helpers/ConstantsTemplate.swift
@@ -18,6 +18,8 @@ struct Constants {
     static var API_KEY = ""
     static var PLAYLIST_ID = ""
     static var API_URL = "https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
-}
  static var VIDEOCELL_ID = "VideoCell"
+ static var YT_EMBED_URL = "https://www.youtube.com/embed/"
+}
+ 
 */
