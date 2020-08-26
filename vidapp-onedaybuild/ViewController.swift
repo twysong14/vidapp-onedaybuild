7ece08033a38f3cd53406bf0f061545fb40aa016
Taylor Powell <taylorpowell@Taylors-MacBook-Pro-4.local>
2020-08-26 10:48:53 -0400

    added custom table cell, configured cell to show video thumbnail, title and date. Also added cache manager to save image data

--- vidapp-onedaybuild/ViewController.swift
+++ vidapp-onedaybuild/ViewController.swift
@@ -48,14 +48,12 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
        
        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath) as! VideoTableViewCell
         
         // Configure the cell with the data
+        let video = self.videos[indexPath.row]
         
-        // Get the title for the video in question
-        let title = self.videos[indexPath.row].title
-        
-        cell.textLabel?.text = title
+        cell.setCell(video)
         
         //Return the cell
         return cell
